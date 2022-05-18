79c076250816e5892f3d4f789e13cc377ffebf2e
shivakumar641 <88086686+shivakumar641@users.noreply.github.com>
2022-05-18 11:44:51 +0530

    Update Statistics.cs

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -7,6 +7,14 @@ namespace Statistics
     {
         public StatsComputer CalculateStatistics(List<float> numbers) {
             //Implement statistics here
+            StatsComputer data = new StatsComputer();
+            if(null == numbers || 0== numbers.Count)
+            {
+                average = 0;
+                min = 0;
+                max = 0;
+            }
+            return data;
         }
     }
 }
